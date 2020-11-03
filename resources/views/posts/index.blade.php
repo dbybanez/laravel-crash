675004e3e5035dd095201fe181723e4e3bfba212
dbybanez <dbybanez@gmail.com>
2020-11-03 22:41:34 +0800

    added retrieve post method for postscontroller, added posts page, added pagination, added single page

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+    @if (count($posts) > 0)
+        @foreach ($posts as $post)
+            <div class="card mb-3">
+              <div class="card-body">
+                <h3 class="mb-0">{{ $post->title }}</h3>
+                <small>Written on {{ $post->created_at }}</small>
+                <a class="stretched-link" href="/posts/{{ $post->id }}"></a>
+              </div>
+            </div>
+        @endforeach
+        {{ $posts->links() }}
+    @else
+        <p>No posts found.</p>
+    @endif
+@endsection
