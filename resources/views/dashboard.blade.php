5dba740e375db65e1714908a703c96ce566ccb2d
dbybanez <dbybanez@gmail.com>
2020-11-06 22:18:49 +0800

    added model relationship in Models\Post.php and Models\User.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,39 @@
                     {{-- {{ __('You are logged in!') }} --}}
                     <a href="/posts/create" class="btn btn-primary mb-3">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    @if (count($posts) > 0)
+                      <table class="table table-bordered table-striped">
+                        <thead>
+                          <tr>
+                            <th>Title</th>
+                            <th width="20%"></th>
+                            <th width="20%"></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($posts as $post)
+                          <tr>
+                            <td class="align-middle"><strong>{{$post->title}}</strong></td>
+                            <td class="text-center align-middle">
+                              <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td class="text-center">
+                              {!!Form::open([
+                                'action' => ['App\Http\Controllers\PostsController@destroy', $post->id],
+                                'method' => 'POST',
+                                'class' => ''
+                              ])!!}
+                                {{Form::hidden('_method', 'DELETE')}}
+                                {{Form::submit('Delete', ['class'=> 'btn btn-danger'])}}
+                              {!!Form::close()!!}
+                            </td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                    @else
+                      <p>You have no posts.</p>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,12 +7,12 @@
             <div class="card mb-3">
               <div class="card-body">
                 <h3 class="mb-0">{{ $post->title }}</h3>
-                <small>Written on {{ $post->created_at }}</small>
+                <small>Written on {{ $post->created_at }} by <strong>{{ $post->user->name }}</strong></small>
                 <a class="stretched-link" href="/posts/{{ $post->id }}"></a>
               </div>
             </div>
         @endforeach
-        {{-- {{ $posts->links() }} --}}
+        {{ $posts->links() }}
     @else
         <p>No posts found.</p>
     @endif
